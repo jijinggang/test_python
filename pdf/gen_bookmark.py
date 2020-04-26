fe142e98727dc86d15911afb55183356ad15a1e3
jijinggang <jijinggang@gmail.com>
2020-04-26 15:53:42 +0800

    add pdf test

---
+++ pdf/gen_bookmark.py
@@ -0,0 +1,28 @@
+import re
+import PyPDF4 as pdf
+
+
+def readtoc(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        regex = re.compile(r"^(\S*[^.])...[\.]+(\d+)$", re.MULTILINE)
+        return regex.findall(text)
+
+
+def main(pdf_file, toc_file, toc_offset, output=None):
+    w = pdf.PdfFileWriter()
+    # with open(pdf_file, 'rb') as rf:
+    r = pdf.PdfFileReader(pdf_file)
+    for page in r.pages:
+        w.addPage(page)
+    tocs = readtoc(toc_file)
+    for toc in tocs:
+        w.addBookmark(toc[0], int(toc[1]) + toc_offset)
+    if(output == None):
+        output = pdf_file
+
+    with open(output, 'wb') as out:
+        w.write(out)
+
+
+main("E:/book/技术/new/CLR+via+C#++第4版.pdf", "./pdf/toc.txt", 15)
