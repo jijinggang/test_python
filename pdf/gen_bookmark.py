cc5657153ac91c3f36cfd51e57b7f20d03310c7d
jijinggang <jijinggang@gmail.com>
2020-04-26 20:38:51 +0800

    fix bug: 正则不正确

--- pdf/gen_bookmark.py
+++ pdf/gen_bookmark.py
@@ -1,19 +1,46 @@
 import re
+import io
+# pip install PyPDF4
 import PyPDF4 as pdf
+# pip install Pillow
+from PIL import Image
 
 
 def readtoc(file):
     with open(file, 'r', encoding='utf-8') as f:
         text = f.read()
-        regex = re.compile(r"^(\S*[^.])...[\.]+(\d+)$", re.MULTILINE)
+        regex = re.compile(r"^([\S| ]*[^.])...[\.]+(\d+)$", re.MULTILINE)
         return regex.findall(text)
+
+
+"""
+def compressImgObj(imgObj):
+    size = (imgObj['/Width'], imgObj['/Height'])
+    data = imgObj.getData()
+    img = Image.open(io.BytesIO(data))
+    img = img.resize((size[0]//2, size[1]//2))
+    imgObj.setData(img.getdata())  # TODO not currently supported
+
+    #img.save(obj[1:] + ext)
+
+
+def deal_page(page):
+    if '/XObject' in page['/Resources']:
+        xObject = page['/Resources']['/XObject'].getObject()
+        for obj in xObject:
+            if xObject[obj]['/Subtype'] == '/Image':
+                compressImgObj(xObject[obj])
+
+    else:
+        pass
+"""
 
 
 def main(pdf_file, toc_file, toc_offset, output=None):
     w = pdf.PdfFileWriter()
-    # with open(pdf_file, 'rb') as rf:
     r = pdf.PdfFileReader(pdf_file)
     for page in r.pages:
+        # deal_page(page)
         w.addPage(page)
     tocs = readtoc(toc_file)
     for toc in tocs:
